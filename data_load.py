b5294739ac6b79b804f47a6ec15a4e024966ff6c
koreateam23 <koreateam23@gmail.com>
2020-10-30 09:27:32 +0900

    10.30 changes

--- data_load.py
+++ data_load.py
@@ -1,43 +1,5 @@
 import numpy as np
-import math
-
-def fni(dl, val):
-    # find nearest index: data list, value
-    idx = np.searchsorted(np.array(dl), val, side='left')
-    if idx > 0 and (idx == len(dl)) or (math.fabs(val - dl[idx - 1]) < math.fabs(val - dl[idx])):
-        return idx - 1
-    else:
-        return idx
-
-def moving_correlation(data1, data2, win_siz=30, mov_siz=30, mode='pearson'):
-    i = 0
-    mc = []
-    while i + win_siz < len(data1):
-        cur1 = data1[i:i+win_siz]
-        cur2 = data2[i:i+win_siz]
-        mc.append(np.corrcoef(cur1, cur2)[0, 1])
-        i += mov_siz
-    return mc
-
-def moving_average(dlc1, dlc2, avg_siz=10):
-    start_pos = dlc1.xlist[0] if dlc1.xlist[0] > dlc2.xlist[0] else dlc2.xlist[0]
-    end_pos = dlc1.xlist[-1] if dlc1.xlist[-1] < dlc2.xlist[-1] else dlc2.xlist[-1]
-    cur_pos = start_pos
-    ylist1 = []
-    ylist2 = []
-    while cur_pos < end_pos:
-        si = fni(dlc1.xlist, cur_pos)
-        ei = fni(dlc1.xlist, cur_pos+avg_siz)
-        if si != ei: ylist1.append(np.average(dlc1.ylist[si:ei]))
-        else: ylist1.append(ylist1[-1])
-
-        si = fni(dlc2.xlist, cur_pos)
-        ei = fni(dlc2.xlist, cur_pos+avg_siz)
-        if si != ei: ylist2.append(np.average(dlc2.ylist[si:ei]))
-        else: ylist2.append(ylist2[-1])
-
-        cur_pos += avg_siz
-    return ylist1, ylist2
+from utils import fni, moving_correlation, moving_average
 
 class Data_list:
     def __init__(self, xlist=None, ylist=None):
@@ -54,27 +16,6 @@ class Data_list:
 
     def count(self):
         return len(self.xlist)
-
-    # def moving_average(self, win_siz=10, mov_siz=10):
-    #     ma_xlist = []
-    #     ma_ylist = []
-    #     pos = self.xlist[0]
-    #     idx = 0
-    #     while True:
-    #         idx = idx + fni(self.xlist[idx:], pos)
-    #         win_end_idx = idx + fni(self.xlist[idx:], pos+win_siz)
-    #         if idx == win_end_idx:
-    #             if idx > len(self.xlist)/10*9: break
-    #             else:
-    #                 ma_xlist.append(pos + (win_siz/2))
-    #                 ma_ylist.append(ma_ylist[-1])
-    #                 pos = pos + mov_siz
-    #                 continue
-    #         cur_y = self.ylist[idx:win_end_idx]
-    #         ma_xlist.append(pos + (win_siz/2))
-    #         ma_ylist.append(sum(cur_y)/len(cur_y))
-    #         pos = pos + mov_siz
-    #     return Data_list(ma_xlist, ma_ylist)
 
     def upper_filtering(self, threshold):
         dl = Data_list()
---
+++ utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import math
+
+def fni(dl, val):
+    # find nearest index: data list, value
+    idx = np.searchsorted(np.array(dl), val, side='left')
+    if idx > 0 and (idx == len(dl)) or (math.fabs(val - dl[idx - 1]) < math.fabs(val - dl[idx])):
+        return idx - 1
+    else:
+        return idx
+
+def moving_correlation(data1, data2, win_siz=30, mov_siz=30, mode='pearson'):
+    i = 0
+    mc = []
+    while i + win_siz < len(data1):
+        cur1 = data1[i:i+win_siz]
+        cur2 = data2[i:i+win_siz]
+        mc.append(np.corrcoef(cur1, cur2)[0, 1])
+        i += mov_siz
+    return mc
+
+def moving_average(dlc1, dlc2, avg_siz=10):
+    start_pos = dlc1.xlist[0] if dlc1.xlist[0] > dlc2.xlist[0] else dlc2.xlist[0]
+    end_pos = dlc1.xlist[-1] if dlc1.xlist[-1] < dlc2.xlist[-1] else dlc2.xlist[-1]
+    cur_pos = start_pos
+    ylist1 = []
+    ylist2 = []
+    while cur_pos < end_pos:
+        si = fni(dlc1.xlist, cur_pos)
+        ei = fni(dlc1.xlist, cur_pos+avg_siz)
+        if si != ei: ylist1.append(np.average(dlc1.ylist[si:ei]))
+        else: ylist1.append(ylist1[-1])
+
+        si = fni(dlc2.xlist, cur_pos)
+        ei = fni(dlc2.xlist, cur_pos+avg_siz)
+        if si != ei: ylist2.append(np.average(dlc2.ylist[si:ei]))
+        else: ylist2.append(ylist2[-1])
+
+        cur_pos += avg_siz
+    return ylist1, ylist2
