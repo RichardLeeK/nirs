97067c5943ae5e3b0ac4b89d1ab63b45f9cb03a6
koreateam23 <koreateam23@gmail.com>
2021-01-11 15:18:41 +0900

    abc

--- data_load.py
+++ data_load.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import fni, moving_correlation, moving_average
+from utils import fni, moving_correlation, moving_average, moving_average_with_null
 
 class Data_list:
     def __init__(self, xlist=None, ylist=None):
@@ -42,9 +42,9 @@ def batch(path, avg_siz=10, win_siz=30):
     art, sco2 = file_loader(path)
     fart = art.upper_filtering(30)
     fsco2 = sco2.upper_filtering(10)
-    art_ylist, sco2_ylist = moving_average(fart, fsco2, avg_siz=avg_siz)
+    # art_ylist, sco2_ylist = moving_average(fart, fsco2, avg_siz=avg_siz)
 
-    v = moving_correlation(art_ylist, sco2_ylist, win_siz=win_siz, mov_siz=win_siz)
+    # v = moving_correlation(art_ylist, sco2_ylist, win_siz=win_siz, mov_siz=win_siz)
     
     # sentence = path + ',' + str(win_siz) + ',' + str(avg_siz)
     # for vv in v:
@@ -53,23 +53,26 @@ def batch(path, avg_siz=10, win_siz=30):
     # file.write(sentence + '\n')
     # file.close()
 
+    art_x, sco2_x, art_y, sco2_y = moving_average_with_null(fart, fsco2, avg_siz=avg_siz)
+     
+
     import matplotlib.pyplot as plt
-    x = list(range(0, len(art_ylist)))
-    x2 = np.linspace(15, x[-1]-15, len(v))
-    _, ax = plt.subplots(nrows=3, ncols=1)
-    ax[0].set_title('COx')
-    
-    ax[0].scatter(x2, v, s=10)
-    ax[1].set_title('MAP')
-    ax[1].scatter(x, art_ylist, c='red', s=3)
-    ax[2].set_title('SCO2')
-    ax[2].scatter(x, sco2_ylist, c='green', s=3)
-    plt.show()
+    # x = list(range(0, len(art_ylist)))
+    # x2 = np.linspace(15, x[-1]-15, len(v))
+    _, ax = plt.subplots(nrows=2, ncols=1, constrained_layout=True)
+    ax[0].set_title('MAP')
+    ax[0].scatter(art_x, art_y, c='red', s=3)
+    ax[1].set_title('SCO2')
+    ax[1].scatter(sco2_x, sco2_y, c='green', s=3)
+    plt.savefig('img/' + path.split('/')[-1].split('.')[0])
 
 import os
 if __name__ == '__main__':
-    batch('data/C4_180111_074321.csv')
-    # files = os.listdir('data')
+    
+    files = os.listdir('data')
+    for file in files:
+        batch('data/' + file)
+
 
     # for file in files:
     #     for i in range(5, 50, 5):
--- utils.py
+++ utils.py
@@ -38,3 +38,24 @@ def moving_average(dlc1, dlc2, avg_siz=10):
 
         cur_pos += avg_siz
     return ylist1, ylist2
+
+def moving_average_with_null(dlc1, dlc2, avg_siz=10):
+    start_pos = dlc1.xlist[0] if dlc1.xlist[0] > dlc2.xlist[0] else dlc2.xlist[0]
+    end_pos = dlc1.xlist[-1] if dlc1.xlist[-1] < dlc2.xlist[-1] else dlc2.xlist[-1]
+    cur_pos = start_pos
+    xlist1 = []; xlist2 = []; ylist1 = []; ylist2 = []
+    while cur_pos < end_pos:
+        si = fni(dlc1.xlist, cur_pos)
+        ei = fni(dlc1.xlist, cur_pos+avg_siz)
+        if si != ei:
+            xlist1.append((cur_pos + end_pos)/2)
+            ylist1.append(np.average(dlc1.ylist[si:ei]))
+
+        si = fni(dlc2.xlist, cur_pos)
+        ei = fni(dlc2.xlist, cur_pos+avg_siz)
+        if si != ei:
+            xlist2.append((cur_pos + end_pos)/2)
+            ylist2.append(np.average(dlc2.ylist[si:ei]))
+        
+        cur_pos += avg_siz
+    return xlist1, xlist2, ylist1, ylist2
